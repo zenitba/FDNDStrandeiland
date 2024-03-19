da740c35443146bde369a324314c46f72cca5c73
zenitba <112856019+zenitba@users.noreply.github.com>
2024-03-19 11:09:22 +0100

    chat werkend gekregen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onNavigate } from '$app/navigation';
   let isVisible = false;
-  let items = []; // Vervang dit door je eigen lijst met items uit je data
+  let items = [];
 
   onNavigate((navigation) => {
     // Check of de browser de ViewTransition API ondersteunt
@@ -15,11 +15,10 @@
     } else {
       // Voer hier een fallback uit, zoals een eenvoudige animatie
       return new Promise((resolve) => {
-        // Simuleer een vertraging voordat de navigatie compleet is
         setTimeout(() => {
           resolve();
           navigation.complete();
-        }, 500); // Aanpassen naar een geschikte vertragingstijd
+        }, 500);
       });
     }
   });
@@ -32,13 +31,13 @@
 <style>
   .fade-item {
     opacity: 0;
-    transform: scale(0.95); /* Start met een iets kleinere schaal */
+    transform: scale(0.95); 
     transition: opacity 500ms ease-out, transform 500ms ease-out;
   }
 
   .visible .fade-item {
     opacity: 1;
-    transform: scale(1); /* Terug naar de normale schaal */
+    transform: scale(1);
   }
 </style>
 
