bcce044618b2dcc0566d3ec538a44eb874ea4483
fatihg61 <112856020+fatihg61@users.noreply.github.com>
2024-04-19 14:29:35 +0200

    :fire:
    Logic fix
    still troubleshooting the filter

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,36 +1,46 @@
-import { gql } from 'graphql-request'
-import { hygraph } from '$lib/utils/hygraph.js'
-
-export async function load() {  
-  let query = gql`
-  query wishes {
-    reactions {
-      id
-      fullname
-      submitField
-      date
-      likeButton
-    }
-    wishes {
-      id
-      heading
-      description
-      date
-      label
-      image {
-        url
-        image {
-          url
-          label
+export async function load() {
+    let query = gql`
+        query wishes {
+            reactions {
+                id
+                fullname
+                submitField
+                date
+                likeButton
+            }
+            wishes {
+                id
+                heading
+                description
+                date
+                label
+                image {
+                    url
+                    image {
+                        url
+                        label
+                    }
+                    label
+                }
+            }
+            sdgs {
+                id
+                image {
+                    url
+                    id
+                }
+            }
         }
-        label
+    `
+
+    const request = await hygraph.request(query)
+
+    const filteredSdgsData = request.sdgs.filter(sdg => sdg.id === 'clqaylmlc7d6t0bw5cimmx01d'); // Filter sdgs data based on a specific condition
+
+    return {
+        data: {
+            sdgs: filteredSdgsData,
+            ...request
         }
-      }
     }
-  `
-
-  const request = await hygraph.request(query)
-
-  return request
-  
 }
