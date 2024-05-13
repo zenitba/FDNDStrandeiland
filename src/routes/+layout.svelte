37be9ef8464d09649424749c80d703cdb42f05a1
zenitba <112856019+zenitba@users.noreply.github.com>
2024-05-13 12:01:53 +0200

    deploy problemen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script>
-	import Viewtransition from "$lib/Organism/viewtransition.svelte";
+	// import Viewtransition from "$lib/Organism/viewtransition.svelte";
 
 </script>
-<Viewtransition />
+<!-- <Viewtransition /> -->
 <slot />
 
 
