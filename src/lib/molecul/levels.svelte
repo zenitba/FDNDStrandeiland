fadc394c01802e84898889aa8d9e43680bfcd2c7
zenitba <112856019+zenitba@users.noreply.github.com>
2024-05-27 21:26:28 +0200

    vercel

--- src/lib/molecul/levels.svelte
+++ src/lib/molecul/levels.svelte
@@ -1,37 +1,56 @@
-<script>
-    import { SvgCost, SvgHeart, SvgRisk } from '$lib';
-    </script>
-    
-    <section class="status-section">
-        <h2 class="status-title">Status wens</h2>
-        
-        <div class="info-item">
-            <p class="info-label">Risiconiveau:</p>
-            <SvgRisk />
-        </div>
-        
-        <div class="info-item">
-            <p class="info-label">Kostenindicatie:</p>
-            <SvgCost />
-        </div>
-        
-        <div class="info-item support">
-            <SvgHeart />
-            <p class="support-text">19 personen vinden dit een goed idee</p>
-        </div>
-    </section>
+<section class="status-section">
+	<h2 class="status-title">Status wens</h2>
+
+	<div class="info-item">
+		<p class="info-label">Risiconiveau:</p>
+		<svg class="icon icon-risk" viewBox="0 0 24 24" width="20" height="20">
+			<path d="M12 9v4" />
+			<path d="M12 16h.01" />
+			<path
+				d="M10.363 3.591l-8.106 13.534a1.914 1.914 0 0 0 1.636 2.871h16.214a1.914 1.914 0 0 0 1.636 -2.87l-8.106 -13.536a1.914 1.914 0 0 0 -3.274 0z"
+			/>
+		</svg>
+	</div>
+
+	<div class="info-item">
+		<p class="info-label">Kostenindicatie:</p>
+		<svg class="icon icon-cost" viewBox="0 0 24 24" width="20" height="20">
+			<path d="M17.2 7a6 7 0 1 0 0 10" />
+			<path d="M13 10h-8m0 4h8" />
+		</svg>
+	</div>
+
+	<div class="info-item support">
+		<svg class="icon icon-heart" viewBox="0 0 24 24" width="20" height="20">
+			<path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+		</svg>
+		<p class="support-text">19 personen vinden dit een goed idee</p>
+	</div>
+</section>
 
 <style>
-    .info-item {
-        display: flex;
-        align-items: center;
-    }
+	.info-item {
+		display: flex;
+		align-items: center;
+	}
 
-    .info-label {
-        margin-right: 10px;
-    }
+	.info-label {
+		margin-right: 10px;
+	}
 
-    .support-text {
-        margin-left: 5px;
-    }
+	.info-item svg {
+		margin-right: -2px;
+		margin-left: -9px;
+		width: 29px;
+		height: 44px;
+		stroke-width: 2.5px;
+		stroke: #ff2825;
+		fill: none;
+		stroke-linecap: round;
+		stroke-linejoin: round;
+	}
+
+	.support-text {
+		margin-left: 5px;
+	}
 </style>
